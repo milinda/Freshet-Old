bec705dfbc6699c99491962a9a3a2afc923dfa91
Milinda Pathirage <milinda.pathirage@gmail.com>
2014-08-12 17:54:25 -0400

    backingmap implementation and aggregator for hourly-topk

--- use-cases/hourly-topk-processes/src/main/java/org/pathirage/cloudbi/topk/HourlyTopKBackingMap.java
+++ use-cases/hourly-topk-processes/src/main/java/org/pathirage/cloudbi/topk/HourlyTopKBackingMap.java
@@ -1,18 +1,93 @@
 package org.pathirage.cloudbi.topk;
 
+import backtype.storm.task.IMetricsContext;
 import com.clearspring.analytics.stream.frequency.CountMinSketch;
+import com.google.common.collect.AbstractIterator;
+import org.apache.commons.lang3.tuple.Pair;
+import redis.clients.jedis.Jedis;
+import storm.trident.state.OpaqueValue;
+import storm.trident.state.State;
+import storm.trident.state.StateFactory;
 import storm.trident.state.map.IBackingMap;
+import storm.trident.state.map.OpaqueMap;
 
-import java.util.List;
+import java.nio.ByteBuffer;
+import java.util.*;
 
-public class HourlyTopKBackingMap implements IBackingMap<CountMinSketch> {
+public class HourlyTopKBackingMap implements IBackingMap<OpaqueValue> {
+    private Jedis redisClient;
+
+    public HourlyTopKBackingMap(Map conf){
+    }
+
+    public static StateFactory FACTORY = new StateFactory() {
+        @Override
+        public State makeState(Map conf, IMetricsContext metrics, int partitionIndex, int numPartitions) {
+            return OpaqueMap.build(new HourlyTopKBackingMap(conf));
+        }
+    };
+
     @Override
-    public List<CountMinSketch> multiGet(List<List<Object>> keys) {
-        return null;
+    public List<OpaqueValue> multiGet(List<List<Object>> keys) {
+        List<String> singleKeys = toSingleKeys(keys);
+        List<OpaqueValue> values = new ArrayList<OpaqueValue>();
+
+        for(String key : singleKeys){
+            Map<byte[], byte[]> fields = redisClient.hgetAll(key.getBytes());
+
+            values.add(new OpaqueValue(
+                    ByteBuffer.wrap(fields.get("txid".getBytes())).getLong(),
+                    CountMinSketch.deserialize(fields.get("curr".getBytes())),
+                    CountMinSketch.deserialize(fields.get("prev".getBytes()))));
+        }
+
+        return values;
     }
 
     @Override
-    public void multiPut(List<List<Object>> keys, List<CountMinSketch> vals) {
+    public void multiPut(List<List<Object>> keys, List<OpaqueValue> vals) {
+        List<String> singleKeys = toSingleKeys(keys);
 
+        for(Pair<String, OpaqueValue> pair : zip(singleKeys, vals)){
+            byte[] prev = CountMinSketch.serialize((CountMinSketch) pair.getValue().getPrev());
+            byte[] cur = CountMinSketch.serialize((CountMinSketch) pair.getValue().getCurr());
+            long currTxid = pair.getValue().getCurrTxid();
+
+            Map<byte[],byte[]> fields = new HashMap<byte[], byte[]>();
+
+            fields.put("txid".getBytes(), ByteBuffer.allocate(8).putLong(currTxid).array());
+            fields.put("prev".getBytes(), prev);
+            fields.put("curr".getBytes(), cur);
+
+            redisClient.hmset(pair.getKey().getBytes(), fields);
+        }
+    }
+
+    public static <L,R> Iterable<Pair<L,R>> zip(final Iterable<L> leftCol, final Iterable<R> rightCol) {
+        return new Iterable<Pair<L,R>>() {
+            @Override
+            public Iterator<Pair<L, R>> iterator() {
+                final Iterator<L> leftItr = leftCol.iterator();
+                final Iterator<R> rightItr = rightCol.iterator();
+                return new AbstractIterator<Pair<L,R>>() {
+                    @Override
+                    protected Pair<L, R> computeNext() {
+                        if (leftItr.hasNext() && rightItr.hasNext()) {
+                            return Pair.of(leftItr.next(), rightItr.next());
+                        } else {
+                            return endOfData();
+                        }
+                    }};
+            }};
+    }
+    private List<String> toSingleKeys(List<List<Object>> keys) {
+        List<String> singleKeys = new ArrayList<String>();
+
+        for(List<Object> key : keys){
+            // Single key because groupBy by hourSinceEpoch
+            singleKeys.add((String)key.get(0));
+        }
+
+        return singleKeys;
     }
 }
---
+++ use-cases/hourly-topk-processes/src/main/java/org/pathirage/cloudbi/topk/HourlyTopKTopologyBuilder.java
@@ -0,0 +1,32 @@
+package org.pathirage.cloudbi.topk;
+
+import backtype.storm.generated.StormTopology;
+import backtype.storm.tuple.Fields;
+import org.pathirage.cloudbi.decoder.ThriftEventDecoderFunction;
+import org.pathirage.cloudbi.utils.HourAssignmentFunction;
+import storm.kafka.ZkHosts;
+import storm.kafka.trident.OpaqueTridentKafkaSpout;
+import storm.kafka.trident.TridentKafkaConfig;
+import storm.trident.Stream;
+import storm.trident.TridentTopology;
+
+public class HourlyTopKTopologyBuilder {
+
+    public static StormTopology buildTopology(String zkHostsStr, String topic){
+        ZkHosts zkHosts = new ZkHosts(zkHostsStr);
+
+        TridentTopology indexerTopology = new TridentTopology();
+
+        TridentKafkaConfig tridentKafkaConfig = new TridentKafkaConfig(zkHosts, topic);
+        OpaqueTridentKafkaSpout kafkaSpout = new OpaqueTridentKafkaSpout(tridentKafkaConfig);
+
+        Stream eventStream = indexerTopology.newStream("events", kafkaSpout);
+        eventStream.each(new Fields("bytes"), new ThriftEventDecoderFunction(), new Fields("stream", "type", "event", "id"))
+                .each(new Fields("stream", "type", "event", "id"), new HourAssignmentFunction(), new Fields("hourSinceEpoch"))
+                .each(new Fields("stream", "type", "event", "id", "hourSinceEpoch"), new InstanceCreatedEventFilter())
+                .project(new Fields("hourSinceEpoch", "process", "instance"))
+                .groupBy(new Fields("hourSinceEpoch"));
+
+        return indexerTopology.build();
+    }
+}
