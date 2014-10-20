d8507251a87176d2e191c4de6660936b2d8b0766
Milinda Pathirage <milinda.pathirage@gmail.com>
2014-10-20 18:50:53 -0400

    dsl project skeleton

---
+++ freshet-dsl/project.clj
@@ -0,0 +1,6 @@
+(defproject org.pathirage.freshet/freshet-dsl "0.1.0-SNAPSHOT"
+  :description "Freshet DSL: Clojure DSL based on CQL."
+  :url "http://github.com/milinda/Freshet"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
